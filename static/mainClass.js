2d01c35292489cce9c3a573a1af9060905e652dd
heyngra <heyngra.wspolpraca@gmail.com>
2022-10-25 16:17:29 +0200

    Add Firefox Support
    This may include Safari support, but I can't confirm it myself, cuz I don't have Safari

--- static/mainClass.js
+++ static/mainClass.js
@@ -16,6 +16,7 @@ export class App
         this.auth_id = generate_random_string(24);
         this.worker = new Worker("static/run_worker.js");
         this.stats = document.getElementById("stats")
+        this.firefox = (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 || (navigator.vendor && navigator.vendor.indexOf('Apple') > -1 && navigator.userAgent && navigator.userAgent.indexOf('CriOS') == -1 && navigator.userAgent.indexOf('FxiOS') == -1)) // firefox is used here as as shorter term for a browser that requires extra support for event.path, see https://github.com/heyngra/TCain-Explorer-Web/issues/4
         this.#fetch();
     }
 
